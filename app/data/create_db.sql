cc35e01d481dfb1f0917c65e535235600030c7c4
leo-turco <leo.turco.83@gmail.com>
2023-11-29 20:26:20 +0100

    chore: :construction: Creation of the SQL script to create and populate the database

---
+++ app/data/create_db.sql
@@ -0,0 +1,56 @@
+BEGIN;
+
+DROP TABLE IF EXISTS "list", "card", "tag", "card_has_tag";
+
+CREATE TABLE "list" (
+  "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "name" TEXT NOT NULL DEFAULT '',
+  "position" INTEGER NOT NULL DEFAULT 0,
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMPTZ
+);
+
+CREATE TABLE "card" (
+  "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "title" TEXT NOT NULL DEFAULT '',
+  "color" TEXT NOT NULL DEFAULT '#FFF' ,
+  "list_id" INTEGER NOT NULL REFERENCES list("id") ON DELETE CASCADE,
+  "position" INTEGER NOT NULL DEFAULT 0,
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMPTZ
+);
+
+CREATE TABLE "tag" (
+  "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "name" TEXT NOT NULL DEFAULT '',
+  "color" TEXT NOT NULL DEFAULT '#FFF' ,
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMPTZ
+);
+
+CREATE TABLE "card_has_tag" (
+  "card_id" INTEGER NOT NULL REFERENCES card("id") ON DELETE CASCADE,
+  "tag_id" INTEGER NOT NULL REFERENCES tag("id") ON DELETE CASCADE,
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+INSERT INTO "list" ("name")
+VALUES ('To do');
+
+INSERT INTO "list" ("name")
+VALUES ('In progress');
+
+INSERT INTO "list" ("name")
+VALUES ('Done');
+
+INSERT INTO "card" ("title", "color", "list_id")
+VALUES ('Learn React', '#087EA4', 1);
+
+INSERT INTO "tag" ("name", "color")
+VALUES ('Urgent', '#ED1C24');
+
+INSERT INTO "card_has_tag" ("card_id", "tag_id")
+VALUES (1,1);
+
+COMMIT;
