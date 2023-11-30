9f1648db11a16aa1808ae3dc9fb8e67d0bc73d7f
leo-turco <leo.turco.83@gmail.com>
2023-11-30 19:49:13 +0100

    feat: :sparkles: Add getOneList function to List controller

--- app/middlewares/listController.js
+++ app/middlewares/listController.js
@@ -25,4 +25,31 @@ const listController = {
       res.status(500).json(error.toString());
     }
   },
+
+  getOneList: async (req, res) => {
+    try {
+      const listId = req.params.id;
+      const list = await List.findByPk(listId, {
+        include: [
+          {
+            association: "cards",
+            include: [
+              {
+                association: "tags",
+              },
+            ],
+          },
+        ],
+        order: [["cards", "position", "ASC"]],
+      });
+      if (list) {
+        res.status(200).json(list);
+      } else {
+        res.status(404).json(`Can't find list ${listId}`);
+      }
+    } catch (error) {
+      console.trace(error);
+      res.status(500).json(error.toString());
+    }
+  },
 };
