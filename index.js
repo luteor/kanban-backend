028fe10e8634c6ce89b30d81a52734a21fc95721
leo-turco <leo.turco.83@gmail.com>
2023-11-30 18:33:22 +0100

    chore: :construction: Added support for JSON and URLencoded formats

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
