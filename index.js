423a79ac7bbe2c83441241f2f3bd1fd3dfa81688
leo-turco <leo.turco.83@gmail.com>
2023-12-01 11:26:23 +0100

    chore: :construction: Setting up the router

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ require("dotenv").config();
 
 const express = require("express");
 
+const router = require("./app/router");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
