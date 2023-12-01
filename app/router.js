5a8ccd7e60448d64f629d771d9a086390840e2ef
leo-turco <leo.turco.83@gmail.com>
2023-12-01 15:30:27 +0100

    feat: :sparkles: Creating routes dedicated to the cardController

--- app/router.js
+++ app/router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const listController = require("./controllers/listController");
+const cardController = require("./controllers/cardController");
 
 const router = express.Router();
 
@@ -13,5 +14,11 @@ router.get("/lists/:id", listController.getOneList);
 router.post("/lists", listController.createList);
 router.put("/lists/:id", listController.modifyList);
 router.delete("/lists/:id", listController.deleteList);
+
+router.get("/lists/:id/cards", cardController.getAllCardsInOneList);
+router.get("/cards/:id", cardController.getOneCard);
+router.post("/cards", cardController.createCard);
+router.put("/cards/:id", cardController.modifyCard);
+router.delete("/cards/:id", cardController.deleteCard);
 
 module.exports = router;
