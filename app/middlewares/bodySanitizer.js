94ad4e77f82d3676962933e54823618827cd1189
leo-turco <leo.turco.83@gmail.com>
2023-12-06 15:53:42 +0100

    fix: :bug: BodySanitizer middleware fixed

--- app/middlewares/bodySanitizer.js
+++ app/middlewares/bodySanitizer.js
@@ -3,7 +3,7 @@ const sanitizer = require("sanitizer");
 const bodySanitizer = (req, res, next) => {
   if (req.body) {
     Object.keys(req.body).forEach((key) => {
-      req.body[body] = sanitizer.escape(req.body[key]);
+      req.body[key] = sanitizer.escape(req.body[key]);
     });
   }
   next();
